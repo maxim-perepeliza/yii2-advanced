ee0a94b7e7440efbfa9083dc9eba4c62071219b7
Maxym Perepelitsja <maximus.perepel96@gmail.com>
2018-09-07 00:20:35 +0300

    Smaller version for symfony components

--- backend/views/cars/_form.php
+++ backend/views/cars/_form.php
@@ -18,7 +18,7 @@ use wbraganca\dynamicform\DynamicFormWidget;
                 'widgetBody' => '.container-items', // required: css class selector
                 'widgetItem' => '.item', // required: css class
                 'limit' => 4, // the maximum times, an element can be cloned (default 999)
-                'min' => 0, // 0 or 1 (default 1)
+                'min' => 1, // 0 or 1 (default 1)
                 'insertButton' => '.add-item', // css class
                 'deleteButton' => '.remove-item', // css class
                 'model' => $modelsBody[0],
